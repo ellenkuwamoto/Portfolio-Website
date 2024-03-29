34f6089e0a280df71727e6e38dd1b77b6acc171a
Ellen Kuwamoto <efk9495@nyu.edu>
2024-03-29 18:11:55 -0400

    content added

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ $(function () {
   })
 
   $("img#3DWorksThumbnail").mouseover(function () {
-    $("img#3DWorksThumbnail").attr("src", "images/3DWorksThumbnailHover.jpg");
+    $("img#3DWorksThumbnail").attr("src", "images/3DWorksThumbnailHover.JPG");
   })
   
   $("img#3DWorksThumbnail").mouseout(function () {
-    $("img#3DWorksThumbnail").attr("src", "images/3DWorksThumbnail.jpg");
+    $("img#3DWorksThumbnail").attr("src", "images/3DWorksThumbnail.JPG");
   })
 
   $("img#interactiveWorksThumbnail").mouseover(function () {
