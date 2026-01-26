7ab5a2eb3afe4e81ef2c16fe0dd14dc8e4eb46c7
Ellen Kuwamoto <efk9495@nyu.edu>
2026-01-26 15:48:31 -0500

    total revamp push #1

--- js/main.js
+++ js/main.js
@@ -1,44 +1,4 @@
 $(function () {
 
-  $("img#2DWorksThumbnail").mouseover(function () {
-    $("img#2DWorksThumbnail").attr("src", "images/2DWorksThumbnailHover.jpg");
-  })
-  
-  $("img#2DWorksThumbnail").mouseout(function () {
-    $("img#2DWorksThumbnail").attr("src", "images/2DWorksThumbnail.jpg");
-  })
-
-  $("img#3DWorksThumbnail").mouseover(function () {
-    $("img#3DWorksThumbnail").attr("src", "images/3DWorksThumbnailHover.JPG");
-  })
-  
-  $("img#3DWorksThumbnail").mouseout(function () {
-    $("img#3DWorksThumbnail").attr("src", "images/3DWorksThumbnail.JPG");
-  })
-
-  $("img#interactiveWorksThumbnail").mouseover(function () {
-    $("img#interactiveWorksThumbnail").attr("src", "images/interactiveWorksThumbnailHover.jpeg");
-  })
-  
-  $("img#interactiveWorksThumbnail").mouseout(function () {
-    $("img#interactiveWorksThumbnail").attr("src", "images/interactiveWorksThumbnail.jpeg");
-  })
-
-  $("img#cellularSoundsThumbnail").mouseover(function () {
-    $("img#cellularSoundsThumbnail").attr("src", "images/cellularSoundsThumbnailHover.jpeg");
-  })
-  
-  $("img#cellularSoundsThumbnail").mouseout(function () {
-    $("img#cellularSoundsThumbnail").attr("src", "images/cellularSoundsThumbnail.jpeg");
-  })
-
-  $("img#slimeMoldScreenThumbnail").mouseover(function () {
-    $("img#slimeMoldScreenThumbnail").attr("src", "images/slimeMoldScreenThumbnailHover.jpeg");
-  })
-  
-  $("img#slimeMoldScreenThumbnail").mouseout(function () {
-    $("img#slimeMoldScreenThumbnail").attr("src", "images/slimeMoldScreenThumbnail.jpeg");
-  })
-
 
 })
